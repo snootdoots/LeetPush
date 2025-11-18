1226ee2942f0151b3e7bfbcd2012882c4fb40eef
David Ren <50275410+snootdoots@users.noreply.github.com>
2025-11-18 11:18:11 -0500

    [15] [Time Beats: 45.54%] [Memory Beats: 71.08%] - LeetPush

--- 3Sum.py
+++ 3Sum.py
@@ -1,25 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        soln = []
-        for i in range(len(nums)):
+        res = []
+        for i in range(len(nums)-2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            k = -nums[i]
-            l, r = i+1, len(nums)-1
-            while l < r:
-                s = nums[l] + nums[r]
-                if s == k:
-                    soln.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r+1]:
-                        r -= 1
-                elif s < k:
-                    l += 1
+            j, k = i+1, len(nums)-1
+            while j < k:
+                curr = nums[i] + nums[j] + nums[k]
+                if curr == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    x, y = nums[j], nums[k]
+                    while j < k and nums[j] == x:
+                        j += 1
+                    while j < k and nums[k] == y:
+                        k -= 1
+                elif curr < 0:
+                    j += 1
                 else:
-                    r -= 1
-        return soln
-                
+                    k -= 1
+        return res
